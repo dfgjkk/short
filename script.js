d6384c6f616d8da85444321d409bd36349a7f6c5
dfgjkk <dautova2009@list.ru>
2023-07-31 17:06:11 +0600

    какое-то сообщение

---
+++ script.js
@@ -0,0 +1,27 @@
+function scrolldown(){
+    window.scroll({top: window.innerHeight, behavior: 'smooth'});
+}
+let button = document.querySelector('.header_btn')
+button.addEventListener('click', scrolldown)
+anime({
+    targets: '.header_left h1',
+    opacity: 1,
+    duration: 500,
+    easing: 'linear'
+})
+
+anime({
+    targets: '.header_left p',
+    opacity: 1,
+    duration: 500,
+    delay: 200,
+    easing: 'linear'
+})
+
+
+anime({
+    targets: '.header_btn',
+    translateY: [-window.innerHeight / 2, 0],
+    duration: 300,
+    easing: 'easeOutQuad'
+})
